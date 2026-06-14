2ee26143eddb44dc589679989433a1011d6a71b4
K.Morita <jhcelium@gmail.com>
2026-06-15 00:06:40 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://onlinesupplements.neoi.jp",
   output: "static",
   trailingSlash: "never",
-  integrations: [sitemap()],
 });
